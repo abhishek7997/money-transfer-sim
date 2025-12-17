8fe627c0f7c1098b8681967c802aa84ac4a856ca
abhishek7997 <68701271+abhishek7997@users.noreply.github.com>
2025-12-17 08:02:45 +0530

    Add thread tasks for simulating transfers

--- src/main/java/org/java/multithread/BankAccount.java
+++ src/main/java/org/java/multithread/BankAccount.java
@@ -3,12 +3,16 @@ package org.java.multithread;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+import java.util.UUID;
+
 @Slf4j
 @Data
 public class BankAccount {
-    private int balance;
+    private final String id;
+    private int balance; // this is the initial balance
 
-    public BankAccount(int balance) {
+    public BankAccount(String id, int balance) {
+        this.id = id != null ? id : UUID.randomUUID().toString();
         this.balance = balance;
     }
 
--- src/main/java/org/java/multithread/Main.java
+++ src/main/java/org/java/multithread/Main.java
@@ -10,30 +10,58 @@ import java.util.concurrent.Executors;
 @Slf4j
 public class Main {
     public static void main(String[] args) {
+        log.info("==== PROGRAM BEGIN ====");
+
         // create 5 bank accounts
         List<BankAccount> bankAccounts = new ArrayList<>();
         for(int i=1;i<=5;i++) {
-            int initialAmount = i*6417 + i*i;
-            bankAccounts.add(new BankAccount(initialAmount));
+            int initialAmount = i*1417 + i*i;
+            bankAccounts.add(new BankAccount(String.valueOf(i), initialAmount));
         }
 
         // create 100 thread (users)
         ExecutorService threadPoolExecutor = Executors.newFixedThreadPool(100);
 
         for(int i=0;i<5;i++) {
+            // pick two accounts
+            BankAccount b1 = bankAccounts.get(i % bankAccounts.size());
+            BankAccount b2 = bankAccounts.get((i + 1) % bankAccounts.size());
+            int amount = 2*i + 3;
 
+            Thread task = new Thread() {
+                @Override
+                public void run() {
+                    try {
+                        transfer(b1, b2, amount);
+                        Thread.sleep(1000);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+
+                private boolean transfer(BankAccount b1, BankAccount b2, int amount) {
+                    log.info("{} : Transferring from {} to {}", Thread.currentThread().getName(), b1.getId(), b2.getId());
+                    log.info("{} : b1.balance={}, b2.balance={}, transferAmount={}", Thread.currentThread().getName(), b1.getBalance(), b2.getBalance(), amount);
+
+                    boolean status = b1.withdraw(amount);
+
+                    if (!status) {
+                        log.error("Insufficient balance.");
+                        return false;
+                    }
+
+                    b2.deposit(amount);
+                    return true;
+                }
+            };
+
+            threadPoolExecutor.submit(task);
         }
+
+        threadPoolExecutor.shutdown();
+
+        log.info("==== PROGRAM END ====");
     }
 
-    private boolean transfer(BankAccount b1, BankAccount b2, int amount) {
-        boolean status = b1.withdraw(amount);
 
-        if (!status) {
-            log.error("Could not withdraw amount. Not enough balance.");
-            return false;
-        }
-
-        b2.deposit(amount);
-        return true;
-    }
 }
